a6acb819f5981cc617451c5dacf1ba07d7011fc6
Harley <nguyenkimtuanduong@gmail.com>
2024-09-01 21:51:38 +0700

    Add: process logic update user info, password, avatar

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,8 @@
+require("dotenv").config();
 import { app } from "./app";
 import connectDB from "./utils/db";
-require("dotenv").config();
+import {v2 as cloudinary} from "cloudinary";
+
 
 // create server
 const PORT = process.env.PORT || 3000;
@@ -9,3 +11,10 @@ app.listen(PORT, () => {
   console.log(`Server is connected with port ${PORT}`);
   connectDB();
 });
+
+// cloudinary config
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_SECRET_KEY
+}); 
