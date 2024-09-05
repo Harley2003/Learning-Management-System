b87821115504a59793b5ebb11f7f23c1a738378a
Harley <nguyenkimtuanduong@gmail.com>
2024-09-05 13:05:15 +0700

    Add: get all notifications, update notification status

---
+++ server/controllers/order.controller.ts
@@ -0,0 +1,97 @@
+import { NextFunction, Request, Response } from "express";
+import ErrorHandler from "../utils/ErrorHandler";
+import { CatchAsyncError } from "../middleware/catchAsyncErrors";
+import OrderModel, { IOrder } from "../models/order.model";
+import NotificationModel from "../models/notification.model";
+import UserModel from "../models/user.model";
+import CourseModel from "../models/course.model";
+import ejs from "ejs";
+import path from "path";
+import sendMail from "../utils/sendMail";
+import { newOrder } from "../services/order.service";
+
+// create order
+export const createOrder = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { courseId, payment_info }: IOrder = req.body;
+
+      const user = await UserModel.findById(req.user?._id);
+
+      const courseExistInUser = user?.courses.some(
+        (course: any) => course._id.toString() === courseId
+      );
+
+      if (courseExistInUser) {
+        return next(
+          new ErrorHandler("You have already purchased this course", 400)
+        );
+      }
+
+      const course = await CourseModel.findById(courseId);
+
+      if (!course) {
+        return next(new ErrorHandler("Course not found", 404));
+      }
+
+      const data: any = {
+        courseId: course?._id,
+        userId: user?._id,
+        payment_info
+      };
+
+      const mailData = {
+        order: {
+          _id: course._id?.toString().slice(0, 6),
+          name: course?.name ?? "",
+          price: course?.price ?? 0,
+          date: new Date().toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric"
+          })
+        }
+      };
+
+      const html = await ejs.renderFile(
+        path.join(__dirname, "../mails/order-confirmation.ejs"),
+        { order: mailData }
+      );
+
+      try {
+        if (user) {
+          await sendMail({
+            email: user.email,
+            subject: "Order Confirmation",
+            template: "order-confirmation.ejs",
+            data: mailData
+          });
+        }
+      } catch (error: any) {
+        return next(new ErrorHandler(error.message, 500));
+      }
+
+      user?.courses.push(course?._id as any);
+
+      await user?.save();
+
+      await NotificationModel.create({
+        user: user?._id,
+        title: "New Order",
+        message: `You have a new order for ${course?.name}`
+      });
+
+      if (course.purchased) {
+        course.purchased += 1;
+      } else {
+        course.purchased = 1;
+      }
+
+      await course.save();
+
+      newOrder(data, res, next);
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
