03cd5d304bf937e24611cfab7524baf260af93de
Harley <nguyenkimtuanduong@gmail.com>
2024-09-06 00:47:12 +0700

    Update: cache maintenance

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -219,7 +219,9 @@ export const updateAccessToken = CatchAsyncError(
       const session = await redis.get(decoded.id as string);
 
       if (!session) {
-        return next(new ErrorHandler(message, 400));
+        return next(
+          new ErrorHandler("Please login to access this resource", 400)
+        );
       }
 
       const user = JSON.parse(session);
@@ -245,6 +247,8 @@ export const updateAccessToken = CatchAsyncError(
       res.cookie("access_token", accessToken, accessTokenOptions);
 
       res.cookie("refresh_token", refreshToken, refreshTokenOptions);
+
+      await redis.set(user._id as string, JSON.stringify(user), "EX", 604800); // 1 week
 
       res.status(200).json({
         status: "success",
