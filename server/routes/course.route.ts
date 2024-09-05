1de7ffbaf2b14f861c0e705472b1e33837499add
Harley <nguyenkimtuanduong@gmail.com>
2024-09-05 15:59:51 +0700

    Add: delete course

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -4,6 +4,7 @@ import {
   addQuestion,
   addReplyToReview,
   addReview,
+  deleteCourse,
   editCourse,
   getAllCourses,
   getAllCoursesAdmin,
@@ -53,6 +54,13 @@ courseRouter.get(
   isAuthenticated,
   authorizeRoles("admin"),
   getAllCoursesAdmin
+);
+
+courseRouter.delete(
+  "/delete-course/:id",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  deleteCourse
 );
 
 export default courseRouter;
