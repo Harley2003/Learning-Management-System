188206e5528f16e6648c6dafc79339fa8fd8dd5f
Harley <nguyenkimtuanduong@gmail.com>
2024-09-05 16:43:36 +0700

    Update: format code

--- server/routes/order.route.ts
+++ server/routes/order.route.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
-import { createOrder, getAllOrdersAdmin } from "../controllers/order.controller";
+import {
+  createOrder,
+  getAllOrdersAdmin
+} from "../controllers/order.controller";
 
 const orderRouter = express.Router();
 
--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,7 @@
 require("dotenv").config();
 import { app } from "./app";
 import connectDB from "./utils/db";
-import {v2 as cloudinary} from "cloudinary";
-
+import { v2 as cloudinary } from "cloudinary";
 
 // create server
 const PORT = process.env.PORT || 3000;
@@ -17,4 +16,4 @@ cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_SECRET_KEY
-}); 
+});
