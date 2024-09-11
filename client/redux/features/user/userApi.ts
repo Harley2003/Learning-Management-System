49f0b91c92de222a0ac0fc799df43fac999cb9f5
Harley <nguyenkimtuanduong@gmail.com>
2024-09-12 00:21:49 +0700

    Update: edit profile (name)

--- client/redux/features/user/userApi.ts
+++ client/redux/features/user/userApi.ts
@@ -1,16 +1,24 @@
 import { apiSlice } from "../api/apiSlice";
 
 export const userApi = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        updateAvatar: builder.mutation({
-            query: (avatar) => ({
-                url: "update-user-avatar",
-                method: "PUT",
-                body: {avatar},
-                credentials: "include" as const
-            })
-        })
+  endpoints: (builder) => ({
+    updateAvatar: builder.mutation({
+      query: (avatar) => ({
+        url: "update-user-avatar",
+        method: "PUT",
+        body: { avatar },
+        credentials: "include" as const
+      })
+    }),
+    editPrilfe: builder.mutation({
+      query: ({ name }) => ({
+        url: "update-user-info",
+        method: "PUT",
+        body: { name },
+        credentials: "include" as const
+      })
     })
-})
+  })
+});
 
-export const { useUpdateAvatarMutation } = userApi;
+export const { useUpdateAvatarMutation, useEditPrilfeMutation } = userApi;
