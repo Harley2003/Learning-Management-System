9f3220c4e9ba90c97fadbb72e23ca521fbfb47e5
Harley <nguyenkimtuanduong@gmail.com>
2024-09-16 11:49:55 +0700

    Add: feature create courses

---
+++ client/app/utils/CoursePlayer.tsx
@@ -0,0 +1,47 @@
+import React, { FC, useState, useEffect } from "react";
+import axios from "axios";
+
+type Props = {
+  videoUrl: string;
+  title: string;
+};
+
+const CoursePlayer: FC<Props> = ({ videoUrl, title }) => {
+  const PLAYER = "O6QJlty4jEt29mdo";
+  const [videoData, setVideoData] = useState({
+    otp: "",
+    playbackInfo: ""
+  });
+
+  useEffect(() => {
+    axios
+      .post(`http://localhost:8000/api/v1/getVdoCipherOTP`, {
+        videoId: videoUrl
+      })
+      .then((res: any) => {
+        setVideoData(res.data);
+      });
+  }, [videoUrl]);
+
+  return (
+    <div className="pt-[41%] relative">
+      {videoData.otp && videoData.playbackInfo !== "" && (
+        <iframe
+          src={`https://player.vdocipher.com/v2/?otp=${videoData?.otp}&playbackInfo=${videoData?.playbackInfo}&player=${PLAYER}`}
+          style={{
+            border: "0",
+            width: "90%",
+            height: "100%",
+            position: "absolute",
+            top: 0,
+            left: 0
+          }}
+          allowFullScreen={true}
+          allow="encrypted-media"
+        ></iframe>
+      )}
+    </div>
+  );
+};
+
+export default CoursePlayer;
