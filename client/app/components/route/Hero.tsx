cffbe48e5e52fa65821a8d301cc305e2b251f141
Harley <nguyenkimtuanduong@gmail.com>
2024-09-17 08:47:59 +0700

    Update: priority of image

--- client/app/components/route/Hero.tsx
+++ client/app/components/route/Hero.tsx
@@ -11,6 +11,7 @@ const Hero: FC<Props> = (props) => {
       <div className="absolute top-[100px] 1000px:top-[unset] 1500px:h-[700px] 1500px:w-[700px] 1100px:h-[600px] 1100px:w-[600px] h-[50vh] w-[50vh] hero_animation rounded-full bg-gradient-to-r from-blue-500 to-blue-600 animate-pulse left-20"></div>
       <div className="1000px:w-[40%] flex 1000px:min-h-screen items-center justify-end pt-[70px] 1000px:pt-0 z-10">
         <Image
+          priority={true}
           src={require("../../../public/assests/banner-img-1.png")}
           alt=""
           className="object-contain 1100px:max-w-[90%] w-[90%] 1500px:max-w-[85%] h-auto z-10"
@@ -41,18 +42,21 @@ const Hero: FC<Props> = (props) => {
         <br />
         <div className="1500px:w-[55%] 1100px:w-[78%] w-[90%] flex items-center">
           <Image
+            priority={true}
             src={require("../../../public/assests/client-1.jpg")}
             alt=""
             className="rounded-full"
           />
 
           <Image
+            priority={true}
             src={require("../../../public/assests/client-2.jpg")}
             alt=""
             className="rounded-full ml-[-20px]"
           />
 
           <Image
+            priority={true}
             src={require("../../../public/assests/client-3.jpg")}
             alt=""
             className="rounded-full ml-[-20px]"
