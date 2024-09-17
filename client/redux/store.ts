a24e9852d361c77265ebc50106883c8911fe59f7
Harley <nguyenkimtuanduong@gmail.com>
2024-09-17 08:48:23 +0700

    Edit: comment refresh token

--- client/redux/store.ts
+++ client/redux/store.ts
@@ -1,5 +1,3 @@
-"use client";
-
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./features/api/apiSlice";
 import authSlice from "./features/auth/authSlice";
@@ -16,10 +14,10 @@ export const store = configureStore({
 
 // call the refresh token function on every page load
 const initializeApp = async () => {
-  await store.dispatch(
-    apiSlice.endpoints.refreshToken.initiate({}, { forceRefetch: true })
-  );
-  
+  // await store.dispatch(
+  //   apiSlice.endpoints.refreshToken.initiate({}, { forceRefetch: true })
+  // );
+
   await store.dispatch(
     apiSlice.endpoints.loadUser.initiate({}, { forceRefetch: true })
   );
